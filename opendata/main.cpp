69773f414b9466e0bf3147feb9c9e0439ccabb1a
ACerisey <acerisey@Debian.myguest.virtualbox.org>
2023-09-21 10:00:50 +0200

    sans erreur mais compile pas

--- opendata/main.cpp
+++ opendata/main.cpp
@@ -11,60 +11,26 @@ typedef struct Datalist Datalist;
 
 struct Datalist{
     int percent;
-    string data;
+    char* data;
 
 };
 
-Datalist* CompileArgument()
+void MakePieChart(gdImagePtr im, gdFontPtr f, vector<Datalist> data, int colors[], int center, int radius)
 {
-    int i;
-    int size = (numArgs-2)/2;
-    Datalist* list;
-    if((numArgs-2)%2!=0 ||  numArgs<6)
-    {
-        printf("NOT ENOUGH ARGUMENT\n");
-        //return 1;
-        list = Init(50, "Placeholder");
-        Add(list, 50, "placeholder2");
-        filename = "test.png";
-        return list;
-    }
-    int sum=0;
-    int tempInt;
-    for(i=2; i<size+1; i++)
-    {
-        tempInt = atoi(args[i]);
-        if(i=2)
-            list=Init(tempInt, args[i+size]);
-        else
-            Add(list, tempInt, args[i+size]);
-        sum+=tempInt;
-    }
-
-    if(sum!=100)
-    {
-        printf("THERE IS DATA MISSING");
-    }
-    filename=args[numArgs];
-    return list;
-}
-
-void MakePieChart(gdImagePtr im, gdFontPtr f, Datalist* data, int colors[], int center, int radius)
-{
-    int len = LastIndex(data);
-    Datalist* currentData;
+    int len = data.size();
+    Datalist currentData;
     int i=0;
     int startP=0;
     int endP;
     for(i=0; i<len+1; i++)
     {
-        currentData=GetMember(data,i);
-        endP=startP+3.6*currentData->percent;
+        currentData=data[i];
+        endP=startP+3.6*currentData.percent;
         gdImageFilledArc(im, center, center, radius*2, radius*2,startP, endP,colors[i],0);
-        if(endP<=180)
-            gdImageString(im, f, center+radius+strlen(currentData->data), center,currentData->data,colors[i]);
+        /*if(endP<=180)
+            gdImageString(im, f, center+radius+strlen(currentData.data), center,currentData.data,colors[i]);
         else
-            gdImageString(im, f, center+(radius/2) , center-radius,currentData->data,colors[i]);
+            gdImageString(im, f, center+(radius/2) , center-radius,currentData.data,colors[i]);*/
         startP=endP;
     }
 }
@@ -99,24 +65,36 @@ int main()
 
     int colors[4] = {black, red, blue,green};
     font = gdFontGetGiant();
-    MakePieChart(im, font, datas, colors, sizeX/2, sizeX/4);
+
     int capacity;
     int bikeused;
     int bikeavailable;
     for(auto& elm : json.items() )
     {
         nlohmann::json object = elm.value();
+
         //cout<<object["name"]<<"\n\n";
-        capacity += object["capacity"];
+        capacity += object["capacity"].get<int>();
         //cout<<object["capacity"]<<"\n\n";
-        bikeavailable += object["num bikes available"];
+        bikeavailable += object["num bikes available"].get<int>();
         //cout<<object["num_bikes_available"]<<"\n\n";
     }
+
     bikeused = capacity-bikeavailable;
-    pngout = fopen(file, "wb");
+    bikeavailable = (bikeavailable/capacity)*100;
+    bikeused = (bikeused/capacity)*100;
+    Datalist bikeusedD;
+    bikeusedD.percent = bikeused;
+    strcpy(bikeusedD.data ,"bikes used");
+    Datalist bikeA;
+    bikeA.percent = bikeavailable;
+    strcpy(bikeA.data, "bikes available");
+    vector<Datalist> datas;
+    datas.push_back(bikeusedD);
+    MakePieChart(im, font, datas, colors, sizeX/2, sizeX/4);
+    pngout = fopen("graph.png", "wb");
     gdImagePng(im, pngout);
     fclose(pngout);
-    free(file);
     gdImageDestroy(im);
     //cout<<json;
 
